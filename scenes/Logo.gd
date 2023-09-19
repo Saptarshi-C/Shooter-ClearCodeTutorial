920ee7e3dcab2d62196ad97b47ffb6408ccc9a60
Saptarshi Chakraborty <saptarshi.office@gmail.com>
2023-09-19 14:45:34 +0530

    Test more GDScript
    * Test for loops, arrays and delta
    * Test influencing other nodes from script

--- scenes/Logo.gd
+++ scenes/Logo.gd
@@ -1,15 +1,15 @@
 extends Sprite2D
 
 var pos: Vector2 = Vector2(300,200)
-const speed: int = 10
+const speed: int = 200
 var test_scale: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position = pos
 	
-	var test_rotation = 45
-	rotation_degrees = test_rotation
+#	var test_rotation = 45
+#	rotation_degrees = test_rotation
 	
 	test_scale = 2
 	scale = Vector2.ONE * test_scale
@@ -17,6 +17,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += speed
-	test_scale += 1
-	scale = Vector2.ONE * test_scale
+	position.x += speed * delta
+#	test_scale += 1
+#	scale = Vector2.ONE * test_scale

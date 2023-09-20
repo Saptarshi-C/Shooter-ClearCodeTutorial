26abddad0da124cd69a00ee029d573e2b3fc31ef
Saptarshi Chakraborty <saptarshi.office@gmail.com>
2023-09-20 15:02:19 +0530

    Test Area2D, CollisionPolygon2D and Signals
    * Create a new scene with a gate with a polygon collision area and an area 2d
    * Create gate script and connect Area2D with it
    * Create a custom signal to emit from gate
    * Connect signal to level script
    * Repeat custom signal process for laser and grenade firing and connect to level script

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+
+func _on_gate_player_entered_gate(body):
+	print(body)
+
+
+func _on_player_fire_laser():
+	print("Firin' ma laser")
+
+
+func _on_player_fire_grenade():
+	print("Boom")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
+
+signal fire_laser
+signal fire_grenade
+
+
 var moveSpeed = 500
+var can_laser: bool = true
+var can_grenade: bool = true
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,9 +24,21 @@ func _process(_delta):
 	move_and_slide()
 	
 	# shoot laser
-	if Input.is_action_pressed("primary action"):
-		print("firin ma laser")
+	if Input.is_action_pressed("primary action") and can_laser:
+		fire_laser.emit()
+		can_laser = false
+		$LaserTimer.start()
 		
 	# shoot grenade
-	if Input.is_action_just_pressed("secondary action"):
-		print("throw grenade")
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		fire_grenade.emit()
+		can_grenade = false
+		$GrenadeTimer.start()
+
+
+func _on_laser_timer_timeout():
+	can_laser = true
+
+
+func _on_grenade_timer_timeout():
+	can_grenade = true
